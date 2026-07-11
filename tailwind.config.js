00c07d9960c5e2d7894a041b2923209ffb390f3b
gbgconsulting <gabgiroto@gmail.com>
2026-07-11 16:25:08 -0300

    feat: implementada tarefa T007 - Config TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './apps/**/templates/**/*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
